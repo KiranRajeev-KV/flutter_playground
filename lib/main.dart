83037a35208dc3c259a6bc5fffe49efe917ac32f
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2025-08-10 22:58:36 +0530

    feat: add ClickCounter widget to main app

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,41 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ClickCounter(),
         ),
       ),
+    );
+  }
+}
+
+class ClickCounter extends StatefulWidget {
+  const ClickCounter({super.key});
+
+  @override
+  State<ClickCounter> createState() => _ClickCounterState();
+}
+
+class _ClickCounterState extends State<ClickCounter> {
+  int _count = 0;
+
+  void _increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('Button pressed $_count times'),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text('Click me'),
+        ),
+      ],
     );
   }
 }
